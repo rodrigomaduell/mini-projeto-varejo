9239db9e3c1b687c9fccb544e9d9572eb1ca437a
Rodrigo Maduell <rodrigomaduell@gmail.com>
2026-05-29 18:58:21 -0300

    feat: sprint 2 - transformação de strings e datetime

--- Miniprojeto_RodrigoMaduell_T2.py
+++ Miniprojeto_RodrigoMaduell_T2.py
@@ -23,11 +23,40 @@ with open('data/Base_Varejo.csv', encoding='latin-1') as f:
 
 # Diagnóstico inicial
 print('=' * 55)
-print('SPRINT 1 - DIAFNÓSTICO INICIAL')
+print('SPRINT 1 - DIAGNÓSTICO INICIAL')
 print('=' * 55)
 print(f'Registros    : {df.shape[0]}')
 print(f'Colunas      : {df.shape[1]}')
 print(f'\nNomes das colunas: \n{df.columns}')
 print(f'\nTipos de dados: \n{df.dtypes}')
+
+
+#===========================================================================
+# SPRINT 2 - Transformação de tipos
+#===========================================================================
+
+# Converter DATA de string para datetime    
+df['DATA'] = pd.to_datetime(df['DATA'], format='%d/%m/%Y', errors="coerce")
+
+#Garantir tipos numéricos corretos
+df['CO_ID'] = pd.to_numeric(df['CO_ID'], errors='coerce')
+df['CL_ID'] = pd.to_numeric(df['CL_ID'], errors='coerce')
+df['CL_EC'] = pd.to_numeric(df['CL_EC'], errors='coerce')
+df['CL_FHL'] = pd.to_numeric(df['CL_FHL'], errors='coerce')
+df['PR_ID'] = pd.to_numeric(df['PR_ID'], errors='coerce')
+
+# Padronizar strings - remover  espaços e aplicar title case
+df['CL_GENERO'] = df['CL_GENERO'].str.strip().str.title()
+df['CL_SEG'] = df['CL_SEG'].str.strip().str.title()
+df['PR_CAT'] = df['PR_CAT'].str.strip().str.title()
+df['PR_NOME'] = df['PR_NOME'].str.strip().str.title()
+
+print('\n' + '=' * 55)
+print('SPRINT 2 - TRANSFORMAÇÃO DE TIPOS')
+print('=' * 55)
+print(f'Tipos após conversão: \n{df.dtypes}')
+print(f'\nAmostra da coluna DATA: \n{df["DATA"].head(3).values}')
+print(f'\nValores únicos em CL_GENERO: {df["CL_GENERO"].unique()}')
+print(f'\nValores únicos em CL_SEG: {df["CL_SEG"].unique()}')
 
 
